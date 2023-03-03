367619fdc080ccb242b8acedc39b435200974db0
Anshuman Narayan <anshumanjf@gmail.com>
2023-03-04 01:14:58 +0530

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,22 +6,24 @@ export default function Navbar() {
     return (
         <div>
 
-            <nav className="navbar navbar-dark bg-primary">
-                <div classNameName="container-fluid">
-                    <Link className="navbar-brand" to="/">Full Stack Application</Link>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid d-flex align-items-start flex-row" style={{ width: "100%" }}>
 
-                    <button className="navbar-toggler"
+
+                    <button className="navbar-toggler p-2"
                         type="button"
                         data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent"
                         aria-expanded="false"
-                        aria-label="Toggle navigation">
+                        aria-label="Toggle navigation"
+                        style={{ maxWidth: "3rem" }}>
                         <span className="navbar-toggler-icon"></span>
                     </button>
-
+                    <Link className="navbar-brand p-2" to="/" style={{ marginRight: "auto" }}>Full Stack Application</Link>
+                    <Link className="btn btn-outline-light ml-auto p-2" to="/adduser">Add User</Link>
                 </div>
-                <Link className="btn btn-outline-light" to="/adduser">Add User</Link>
+
 
             </nav>
 
